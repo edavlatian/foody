273f272cbb60528b0292c0b1190a3b42ea9deb19
Edouard Davlatian <chaosmage89+git@gmail.com>
2021-05-25 07:59:52 -0400

    Local Initial Commit
    Initial commit from local repo.

---
+++ public-api/app/Http/Controllers/ReportsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    private $privateApiUrl;
+
+    public function __construct()
+    {
+        $this->privateApiUrl = env('PRIVATE_API_URL');
+    }
+
+    public function mostConsumedNutrient(int $userId)
+    {
+        $response = Http::get("{$this->privateApiUrl}/users/{$userId}/reports/most-consumed-nutrient");
+        return $response->json();
+    }
+
+}
